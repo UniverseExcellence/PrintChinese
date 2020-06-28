b8a5df95aef0fbf726c7c25320810a38600a80df
CE <chinaexcellence@aliyun.com>
2020-06-28 10:17:27 +0800

    项目提交

---
+++ CEPrintChinese/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CEPrintChinese
+//
+//  Created by zhouzhongliang on 2020/6/28.
+//  Copyright © 2020 zhouzhongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
